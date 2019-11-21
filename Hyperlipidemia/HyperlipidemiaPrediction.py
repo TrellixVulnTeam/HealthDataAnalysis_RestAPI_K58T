770fcdb0394038e7603749b2ef34e3c5fe0eb16b
DESKTOP-3BGEHE7\Henock <henockmamo54@gmail.com>
2019-11-21 13:34:12 +0900

    hyper lipidimia models modified

--- Hyperlipidemia/HyperlipidemiaPrediction.py
+++ Hyperlipidemia/HyperlipidemiaPrediction.py
@@ -16,19 +16,18 @@ def predictHyperlipidemiaClass(data):
     
     try:
         # required columns 
-        cols=['AGE','S000300','L100700', 'L100800', 'L100100', "S000501","S000502",'L100200','L101300',
-              'L190000','L100400','L101700',"L190300","L190500","L190400",'L101600', 'L105200']
         
-    #    data=data[cols]
-        
+        cols=['AGE','SEX', 'L100700', 'S000300','L101700', 'L100800', 'L101300',   'L190300', 'L190500', 
+                 'FIELD_33','FIELD_38','FIELD_40']
+         
         with open('Hyperlipidemia/HyperlipidemiaClassifierModel', 'rb') as f:
             rf = pickle.load(f)
             
         data=data[cols] # extract features from the payload data
         
         #rename the columns to match the orginal model features name
-        data.columns=['AGE', 'BMI', 'Uric Acid', 'fasting glucose','L100100','SBP','DBP','L100200','SGPT', 
-                     'L190000','L100400','r-GTP gamma', 'RBC', 'HCT','Hemoglobin', 'L101600', 'L105200']
+        data.columns=['AGE', 'SEX', 'Uric Acid', 'BMI', 'r-GTP gamma', 'fasting glucose','SGPT', 'RBC', 'HCT', '_4_1_Smoking', 'Drinking_5_1_days',
+       '_6_1_of_physical_activity_high_strength']
         
         # compute class probability
         classprobapred= pd.DataFrame(rf.predict_proba(data), columns=['CLASS 0','CLASS 1']).to_json(orient='index')
@@ -56,7 +55,8 @@ def predictHyperlipidemiaNextYearValue(data):
     #    to predict next year values this year value and the features used to predict next year value should not be null    
          
         # required columns to predict class status of the patient
-        cols=['L100700','S000300','L101700','L100800','L103300','L103100','FIELD_33','FIELD_38','FIELD_40','SEX','AGE']
+        cols=['L190500', 'L190300', 'L101300','L100700', 'S000300', 'L101700', 'L100800','L103300', 'L103100',
+          'FIELD_33', 'FIELD_38', 'FIELD_40','SEX', 'AGE']
         
     
     #    features required to generate next year L100700 value
@@ -84,6 +84,22 @@ def predictHyperlipidemiaNextYearValue(data):
     'L100700', 'L190400', 'L190500', 'L100500', 'S000100', 'L101300']
         
         
+        
+    #    features required to generate next year L100800 value
+        cols_L101300=['L101300', 'SEX', 'S000300', 'L101200', 'L103300', 'L101700',
+       'L100700', 'L103100', 'L190400', 'L190300', 'S000100', 'L190500'] 
+        
+    #    features required to generate next year L103300 value
+        cols_L190300=['L190300', 'SEX', 'L190500', 'L190400', 'L103100', 'S000300',
+       'L100700', 'L103300', 'S000100', 'L103000', 'S000501', 'S000502' ]
+        
+    #    features required to generate next year L103100 value
+        cols_L190500=['L190500', 'SEX',  'L190400', 'L190300', 'L104500', 'L190800',
+       'S000300', 'L100700', 'L103100', 'S000100', 'S000502', 'S000501']
+        
+        
+        
+        
         #Load the trained regression models for the next year
         with open('Hyperlipidemia/Models_NextYear/hyperlipidemiaModel_nextyear_L100700_RF_regressor', 'rb') as f:
             reg_L100700 = pickle.load(f)
@@ -104,6 +120,17 @@ def predictHyperlipidemiaNextYearValue(data):
             reg_L103100 = pickle.load(f)
             
             
+            
+        with open('Hyperlipidemia/Models_NextYear/hyperlipidemiaModel_nextyear_L101300_RF_regressor', 'rb') as f:
+            reg_L101300 = pickle.load(f)
+            
+        with open('Hyperlipidemia/Models_NextYear/hyperlipidemiaModel_nextyear_L190300_RF_regressor', 'rb') as f:
+            reg_L190300 = pickle.load(f)
+            
+        with open('Hyperlipidemia/Models_NextYear/hyperlipidemiaModel_nextyear_L190500_RF_regressor', 'rb') as f:
+            reg_L190500 = pickle.load(f)
+            
+            
         
     #    # predicte next year value    
         predictedNextYearValues=pd.DataFrame()
@@ -113,6 +140,11 @@ def predictHyperlipidemiaNextYearValue(data):
         predictedNextYearValues['L100800']=list(reg_L100800.predict(data[cols_L100800]))
         predictedNextYearValues['L103300']=list(reg_L103300.predict(data[cols_L103300]))
         predictedNextYearValues['L103100']=list(reg_L103100.predict(data[cols_L103100]))
+        
+        predictedNextYearValues['L101300']=list(reg_L101300.predict(data[cols_L101300]))
+        predictedNextYearValues['L190300']=list(reg_L190300.predict(data[cols_L190300]))
+        predictedNextYearValues['L190500']=list(reg_L190500.predict(data[cols_L190500]))
+        
         predictedNextYearValues['SEX']=list(data['SEX'])
         predictedNextYearValues['AGE']=list(data['AGE']+1)
         predictedNextYearValues['FIELD_33']=list(data['FIELD_33'])
@@ -126,7 +158,7 @@ def predictHyperlipidemiaNextYearValue(data):
             
         with open('Hyperlipidemia/Models_NextYear/HyperlipidemiaClassifierModelForNextYear_scaler_SMOTE', 'rb') as f:
             _nextyearData_scaler = pickle.load(f)
-            
+                        
         scaledData= _nextyearData_scaler.transform(predictedNextYearValues)
         
         
@@ -148,7 +180,7 @@ def predictHyperlipidemiaNextYearValue(data):
     except Exception as e:
         return ((str(e))+ ' Required Fields: '+ ', '.join(str(x) for x in 
                  np.unique(np.concatenate((cols_L100700,cols_S000300,cols_L101700,
-                cols_L100800,cols_L103300,cols_L103100)))                 
+                cols_L100800,cols_L103300,cols_L103100,cols_L101300,cols_L190300,cols_L190500)))                 
                  ))
                  
 
@@ -157,7 +189,9 @@ def predictNextYearHyperlipidemiaClass(data):
     
     try:            
         # required columns to predict class status of the patient
-        cols=['L100700','S000300','L101700','L100800','L103300','L103100','FIELD_33','FIELD_38','FIELD_40','SEX','AGE']
+#        cols=['L100700','S000300','L101700','L100800','L103300','L103100','FIELD_33','FIELD_38','FIELD_40','SEX','AGE']
+        cols=['L190500', 'L190300', 'L101300','L100700', 'S000300', 'L101700', 'L100800','L103300', 'L103100',
+          'FIELD_33', 'FIELD_38', 'FIELD_40','SEX', 'AGE']
         
         # extract features from the payload data
         data=data[cols]
